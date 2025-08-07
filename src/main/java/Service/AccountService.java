3ca8ff9487a801fcb4d0110f844bd5012a7cc536
Christopher Jackson <c.jackson629@gmail.com>
2025-08-07 16:03:16 +0000

    new fixes for messages

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -13,10 +13,10 @@ public class AccountService {
         this.accountValidator = new AccountValidator();
     }
 
-    public AccountService(AccountDAO accountDAO) {
-        this.accountDAO = accountDAO;
-        this.accountValidator = new AccountValidator();
-    }
+    // public AccountService(AccountDAO accountDAO) {
+    //     this.accountDAO = accountDAO;
+    //     this.accountValidator = new AccountValidator();
+    // }
 
     // add account checks for reuirements of username before calling DAO to add
     // account
--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -2,7 +2,6 @@ package Service;
 
 import DAO.AccountDAO;
 import DAO.MessageDAO;
-import Model.Account;
 import Model.Message;
 import Util.MessageValidator;
 
@@ -12,17 +11,20 @@ public class MessageService {
     MessageValidator messageValidator;
 
     public MessageService() {
-    }
-
-    public MessageService(MessageDAO messageDAO,
-            AccountDAO accountDAO) {
-        this.messageDAO = messageDAO;
-        this.accountDAO = accountDAO;
+        this.messageDAO = new MessageDAO();
+        this.accountDAO = new AccountDAO();
         this.messageValidator = new MessageValidator();
     }
 
+    // public MessageService(MessageDAO messageDAO,
+    //         AccountDAO accountDAO) {
+    //     this.messageDAO = messageDAO;
+    //     this.accountDAO = accountDAO;
+    //     this.messageValidator = new MessageValidator();
+    // }
+
     public Message addMessage(Message message) {
-        if (this.messageValidator.isValidMessage(message) && !accountDAO.accountExists(message.getPosted_by()))
+        if (this.messageValidator.isValidMessage(message) && !this.accountDAO.accountExists(message.getPosted_by()))
             return messageDAO.insertMessage(message);
         return null;
     }
